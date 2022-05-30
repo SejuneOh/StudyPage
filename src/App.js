b2f63c98544306d9db406cd275ad77d7fc2a0127
SejuneOh <etry0715@gmail.com>
2022-05-30 23:50:12 +0900

    feat: add study amin sturct with css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
+
+const Container = styled.div`
+  border: 4px solid blue;
+  height: 100vh;
+`;
+
+const Ul = styled.ul`
+  border: 2px solid red;
+  height: 120px;
+  display: flex;
+  align-content: center;
+  
+
+`;
+
+const Li = styled.li`
+  // border: 2px solid red;
+  line-height: 120px;
+  hight: 10px;
+  list-style-type: none;
+  margin-left: 50px;
+  box-sizing: border-box;
+  font-size: 20px;
+  font-weight: 700;
+ `;
+
+
+// main page li styled
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Ul>
+          <Li>test</Li>
+          <Li>test</Li>
+          <Li>test</Li>
+        </Ul>
+        <Ul></Ul>
+        <Ul></Ul>
+      </Container>
+    </>
   );
 }
 
