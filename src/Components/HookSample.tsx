43fc92da004e7b0359bd9aa7916b7080797d4f12
SejuneOh <etry0715@gmail.com>
2022-05-31 01:57:26 +0900

    feat: Add hook sample

--- src/Components/HookSample.tsx
+++ src/Components/HookSample.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function HookSample() {
-  return <div>HookSample</div>;
+  const [value, setValue] = React.useState("");
+  const [time, setTime] = React.useState(3);
+  const [intervalFlag, setInervalFlag] = React.useState(false);
+
+  const inputChange = (e) => {
+    setValue(e.target.value);
+    setTime(3);
+    setInervalFlag(true);
+  };
+
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime - 1);
+    }, 1000);
+
+    const timer = setTimeout(() => {
+      if (intervalFlag) {
+        console.log("value:", value);
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(timer);
+      clearInterval(interval);
+    };
+  }, [value]);
+
+  return (
+    <>
+      <input type="number" onChange={inputChange} />
+      <br />
+      <span>{time <= 0 ? `log 반영완료` : `${time}초 후 로그 찍힘`} </span>
+    </>
+  );
 }
